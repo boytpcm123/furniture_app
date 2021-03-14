1fb5f4d1437b46f9141628bb6943e8473c4277f8
Thong Nguyen <thong.nguyen@hdwebsoft.co>
2021-03-14 13:23:22 +0700

    Upgrade to new flutter version

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:furniture_app/components/title_text.dart';
+import 'package:furniture_app/screens/home/components/categories.dart';
+import 'package:furniture_app/services/fetchCategories.dart';
 
 import '../../../size_config.dart';
 
@@ -11,12 +13,30 @@ class Body extends StatelessWidget {
     final defaultSize = SizeConfig.defaultSize;
     return SingleChildScrollView(
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Padding(
             padding: EdgeInsets.all(defaultSize * 2), //20
             child: const TitleText(
               title: 'Browse by Categories',
             ),
+          ),
+          FutureBuilder(
+            future: fetchCategories(),
+            builder: (context, snapshot) => snapshot.hasData
+                ? Categories(
+                    categories: snapshot.data,
+                  )
+                : Center(
+                    child: Image.asset(
+                      'assets/ripple.gif',
+                    ),
+                  ),
+          ),
+          Divider(height: 5),
+          Padding(
+            padding: EdgeInsets.all(defaultSize * 2), //20
+            child: TitleText(title: "Recommands For You"),
           )
         ],
       ),
